819598cca701756d76ea5cdec91adc01c8be09a5
mehearaz <mu.himel@gmail.com>
2025-12-03 17:50:43 +0600

    init

---
+++ src/main/java/dev/himel/log_anomaly_summarizer/LogAnomalySummarizerApplication.java
@@ -0,0 +1,13 @@
+package dev.himel.log_anomaly_summarizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogAnomalySummarizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogAnomalySummarizerApplication.class, args);
+	}
+
+}
